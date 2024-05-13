368d5e136d480a59f0298b2f38894056c006efe6
monchitos822 <monchitos822@gmail.com>
2024-05-13 11:51:25 +0200

    v0.8.2TitleScreenFinished
    Terminada la pantalla inicial con el menú totalmente funcional

--- Proyecto_Trabajo_Final/Assets/Scripts/TitleScreen.cs
+++ Proyecto_Trabajo_Final/Assets/Scripts/TitleScreen.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Jugar()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Salir()
     {
-        
+        Debug.Log("Salir...");
+        Application.Quit();
     }
 }
