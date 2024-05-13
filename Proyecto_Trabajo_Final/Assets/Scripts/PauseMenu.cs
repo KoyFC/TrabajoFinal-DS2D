19acd399a567096e360fcb3dfef8755233f8dca1
monchitos822 <monchitos822@gmail.com>
2024-05-13 13:15:52 +0200

    v0.9.0PauseMenu
    Menú de pausa terminado con opciones para reiniciar el nivel y cerrar el juego

---
+++ Proyecto_Trabajo_Final/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseButton;
+    public GameObject PauseMenuO;
+    private bool GamePaused = false;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        GamePaused = true;
+        Time.timeScale = 0f;
+        PauseButton.SetActive(false);
+        PauseMenuO.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        GamePaused = false;
+        Time.timeScale = 1f;
+        PauseButton.SetActive(true);
+        PauseMenuO.SetActive(false);
+    }
+
+    public void Reset()
+    {
+        GamePaused = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Close()
+    {
+        Debug.Log("Closing game");
+        Application.Quit();
+    }
+}
+    
