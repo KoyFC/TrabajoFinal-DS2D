f87eb0bea8508ba179babd09c6b337735e30510b
monchitos822 <monchitos822@gmail.com>
2024-05-29 12:04:46 +0200

    v0.25.3 Volume slider adjustment and level scrip merge

---
+++ Proyecto_Trabajo_Final/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void Level1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Salir...");
+        Application.Quit();
+    }
+}
